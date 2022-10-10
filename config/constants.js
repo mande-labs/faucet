63bff6d1ad71e906ece3453346e0f0e06e47a760
Pratik Agarwal <prtkagarwal15@gmail.com>
2022-10-11 03:29:35 +0530

    config update

--- config/constants.js
+++ config/constants.js
@@ -1,15 +1,14 @@
 
 const FAUCET_QUEUE_LIMIT = 15;
-const AMOUNT = "100";
+const AMOUNT = "100000000";
 const DENOM = "mand";
-const CHAIN_ID = "mandechain";
+const CHAIN_ID = "mande-testnet-1";
 let faucetQueue=[];
-const prefix= "cosmos";
-const gas_price = "0.025mand";
+const prefix= "mande";
+const gas_price = "0.0025mand";
 const gas = "20000000";
-const TIME_LIMIT = 3600; // 3600 seconds = 1 hours
-const MAX_PER_IP = 100;
-const MAX_VOTE_PER_IP = 20;
+const TIME_LIMIT = 24*3600; // 1 day
+const MAX_PER_IP = 1;
 const HD_PATH = "m/44'/118'/0'/0/0";
 module.exports = {
     FAUCET_QUEUE_LIMIT,
@@ -22,6 +21,5 @@ module.exports = {
     gas,
     TIME_LIMIT,
     MAX_PER_IP,
-    MAX_VOTE_PER_IP,
     HD_PATH
 }
